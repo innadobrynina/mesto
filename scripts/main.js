85f69efc77f8da97c98e8c096d026ec54ba325cd
Inna Dobrynina <a-ya-dobraya@yandex.ru>
2021-01-31 17:41:37 +0500

    3 проверка

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,11 @@
 let formElement = document.querySelector('.popup');
-let editButton = document.querySelector('.profile__edit-button');
+let editButton = document.querySelector('.profile__button-edit');
 let popupCloseButton = formElement.querySelector('.popup__close');
-let nameInput = formElement.querySelector('.popup__name');
-let aboutInput = formElement.querySelector('.popup__about');
+let nameInput = form.elements.title;
+let aboutInput = form.elements.about;
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 let saveButton = formElement.querySelector('.popup__button');
-
 
 // Функция открытия окна редактирования
 function fEditButton() {
@@ -15,13 +14,10 @@ function fEditButton() {
     formElement.classList.add('popup_opened');
 }
 
-// Вызов функции открытия окна редактирования
-editButton.addEventListener('click', fEditButton);
-
-// Закрытие окна по кнопке
-popupCloseButton.addEventListener('click', function() {
+//Функция закрытия окна редактирования
+function fCloseButton() {
     formElement.classList.remove('popup_opened');
-});
+}
 
 // Функция кнопки сохранения
 function formSubmitHandler(evt) {
@@ -30,6 +26,12 @@ function formSubmitHandler(evt) {
     profileSubtitle.textContent = aboutInput.value
     formElement.classList.remove('popup_opened');
 }
+
+// Вызов функции открытия окна редактирования
+editButton.addEventListener('click', fEditButton);
+
+// Вызов функции закрытия окна редактирования по кнопке
+popupCloseButton.addEventListener('click', fCloseButton);
 
 // Сохранение результата работы функции
 formElement.addEventListener('submit', formSubmitHandler);
